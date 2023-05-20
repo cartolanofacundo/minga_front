b1bc3d9f8848701d9c72eb18573b3f87d5f32ed1
cartolanofacundo <cartolanofacundo@gmail.com>
2023-05-20 18:45:59 -0300

    isDesktop tinyPng hero_image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import { Carousel } from "../components/Home/Carousel"
 import { Hero } from "../components/Home/Hero"
+import { useMediaQuery } from "react-responsive"
 
 export function Home() {
+    const isDesktop = useMediaQuery({
+        query: '(min-width: 1224px)'
+      })
     return (
         <div className="grow w-full h-screen">
             <Hero/>
-            <Carousel/>
+            {isDesktop && <Carousel/> }
         </div>
     )
 }
