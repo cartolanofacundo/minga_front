0d8bae630ff33f5870aa5320b9ec109db50bd197
cartolanofacundo <cartolanofacundo@gmail.com>
2023-05-21 20:42:43 -0300

    become-member respoonsive

--- src/assets/icons/Icons.jsx
+++ src/assets/icons/Icons.jsx
@@ -46,3 +46,19 @@ export function Spinner() {
         </svg>
     )
 }
+export function ChevronRight() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+        </svg>
+
+    )
+}
+export function CheckIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-[12px] h-[12px]">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+        </svg>
+
+    )
+}
