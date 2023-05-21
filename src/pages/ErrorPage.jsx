2d8055b875150fb56062876c700cce5ef68c8194
cartolanofacundo <cartolanofacundo@gmail.com>
2023-05-21 18:58:43 -0300

    error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,6 +1,12 @@
+import { Link } from "react-router-dom";
+
 export function ErrorPage(){
     //todo error
     return(
-        <h1>Error</h1>
+        <div className="flex flex-col justify-center items-center w-screen h-screen bg-[#4338CA] text-white">
+            <p className="font-poppins font-bold text-9xl ">404</p>
+            <h1 className="font-poppins font-medium text-4xl">The page you’re looking for <br /> can’t be found.</h1>
+            <Link to={"/"} className="text-2xl mt-48 underline" >Go to home page</Link>
+        </div>
     )
 }
