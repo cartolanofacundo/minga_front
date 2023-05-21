afcbe7fd34fe0ef19403e67a2152a6aa576fd031
cartolanofacundo <cartolanofacundo@gmail.com>
2023-05-21 19:07:00 -0300

    fix css errorPage for mobile

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 export function ErrorPage(){
     //todo error
     return(
-        <div className="flex flex-col justify-center items-center w-screen h-screen bg-[#4338CA] text-white">
+        <div className="flex flex-col justify-center items-center w-screen h-screen bg-[#4338CA] text-white p-8">
             <p className="font-poppins font-bold text-9xl ">404</p>
-            <h1 className="font-poppins font-medium text-4xl">The page you’re looking for <br /> can’t be found.</h1>
+            <h1 className="font-poppins font-medium text-4xl text-center">The page you’re looking for <br /> can’t be found.</h1>
             <Link to={"/"} className="text-2xl mt-48 underline" >Go to home page</Link>
         </div>
     )
