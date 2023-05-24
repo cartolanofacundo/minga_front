b7c32237ca2a281a95e11c1d8a9c5817ba4209d9
cartolanofacundo <cartolanofacundo@gmail.com>
2023-05-24 03:51:57 -0300

    nothing to show

--- src/components/explore-mangas/MangasMap.jsx
+++ src/components/explore-mangas/MangasMap.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux"
 import { MangaCard } from "./MangaCard"
+import { NothingToShow } from "./NothingToShow"
 
 export function MangasMap() {
     let { mangas } = useSelector(store => store.mangas)
@@ -7,9 +8,10 @@ export function MangasMap() {
         <>
         <div className="grow flex flex-row gap-x-14 gap-y-4 flex-wrap m-auto w-full justify-center mt-8">
             {
+            mangas?.length > 0 ?
             mangas?.map((manga) => {
                 return <MangaCard key={manga._id} manga={manga} />
-            })}
+            }) : <NothingToShow/>}
         </div>
         </>
         
